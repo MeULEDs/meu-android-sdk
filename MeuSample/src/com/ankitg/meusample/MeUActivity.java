c8b964f03b67bbe7acd59e9304fe037df8a3d31a
Ankit Guglani <ankitg@gmail.com>
2014-12-13 00:00:21 -0500

    - Updated to version 0.2 - Made major revisions to the sample app, included colour picker and scrolling image picker. - Cleaned up SDK implementation.

--- MeuSample/src/com/ankitg/meusample/MeUActivity.java
+++ MeuSample/src/com/ankitg/meusample/MeUActivity.java
@@ -5,18 +5,39 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.ankitguglani.MeU;
+import com.larswerkman.holocolorpicker.ColorPicker;
 
-public class MeUActivity extends Activity {
+public class MeUActivity extends Activity implements OnClickListener {
 
 	Button btn_connect;
+//	Button btn_disconnect;
+	Button btn_refresh;
 	Button btn_send_text;
-	ImageButton btn_send_image;
+	ImageButton btn_send_image1;
+	ImageButton btn_send_image2;
+	ImageButton btn_send_image3;
+	ImageButton btn_send_image4;
+	ImageButton btn_send_image5;
+	ImageButton btn_send_image6;
+	ImageButton btn_send_image7;
+	ImageButton btn_send_image8;
+	ImageButton btn_send_image9;
+	ImageButton btn_send_image10;
+	ImageButton btn_send_image11;
+	ImageButton btn_send_image12;
+	TextView tv_bt_status;
 	EditText et_text;
+	Spinner spn_bluetooth;
+	ColorPicker picker;
 	
 	MeU MeU = new MeU();
 	
@@ -25,26 +46,158 @@ public class MeUActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meu);
         
-        btn_connect = (Button)findViewById(R.id.BTN_connect);
-        btn_send_text = (Button)findViewById(R.id.BTN_sendText);
-        btn_send_image = (ImageButton)findViewById(R.id.BTN_main_image1);
-        et_text = (EditText)findViewById(R.id.ET_main);
+        init();
     }
     
-    public void connect(View v)
-    {
-    	MeU.setupBluetooth(getApplicationContext(), "00:06:66:6A:CB:9A"); // Change this to the SSID of your MeU
+    private void init() {
+        btn_connect = (Button)findViewById(R.id.BTN_main_connect);
+//        btn_disconnect = (Button)findViewById(R.id.BTN_main_disconnect);
+        btn_refresh = (Button)findViewById(R.id.BTN_main_refresh);
+        btn_send_text = (Button)findViewById(R.id.BTN_main_sendText);
+        btn_send_image1 = (ImageButton)findViewById(R.id.BTN_main_image1);
+        btn_send_image2 = (ImageButton)findViewById(R.id.BTN_main_image2);
+        btn_send_image3 = (ImageButton)findViewById(R.id.BTN_main_image3);
+        btn_send_image4 = (ImageButton)findViewById(R.id.BTN_main_image4);
+        btn_send_image5 = (ImageButton)findViewById(R.id.BTN_main_image5);
+        btn_send_image6 = (ImageButton)findViewById(R.id.BTN_main_image6);
+        btn_send_image7 = (ImageButton)findViewById(R.id.BTN_main_image7);
+        btn_send_image8 = (ImageButton)findViewById(R.id.BTN_main_image8);
+        btn_send_image9 = (ImageButton)findViewById(R.id.BTN_main_image9);
+        btn_send_image10 = (ImageButton)findViewById(R.id.BTN_main_image10);
+        btn_send_image11 = (ImageButton)findViewById(R.id.BTN_main_image11);
+        btn_send_image12 = (ImageButton)findViewById(R.id.BTN_main_image12);
+        tv_bt_status = (TextView)findViewById(R.id.TV_main_bluetooth_status);
+        et_text = (EditText)findViewById(R.id.ET_main_text);
+        spn_bluetooth = (Spinner)findViewById(R.id.SPN_main_bluetooth_list);
+        picker = (ColorPicker)findViewById(R.id.Picker_main_colorpicker);
+        
+        btn_connect.setOnClickListener(this);
+//        btn_disconnect.setOnClickListener(this);
+        btn_refresh.setOnClickListener(this);
+        btn_send_image1.setOnClickListener(this);
+        btn_send_image2.setOnClickListener(this);
+        btn_send_image3.setOnClickListener(this);
+        btn_send_image4.setOnClickListener(this);
+        btn_send_image5.setOnClickListener(this);
+        btn_send_image6.setOnClickListener(this);
+        btn_send_image7.setOnClickListener(this);
+        btn_send_image8.setOnClickListener(this);
+        btn_send_image9.setOnClickListener(this);
+        btn_send_image10.setOnClickListener(this);
+        btn_send_image11.setOnClickListener(this);
+        btn_send_image12.setOnClickListener(this);
+        btn_send_text.setOnClickListener(this);
+        
+        picker.setShowOldCenterColor(false);
+        populateBtDevices();
+        updateStatus();
     }
     
-    public void sendText(View v)
+    private void updateStatus()
     {
-    	MeU.sendText(et_text.getText().toString(), "FF0000");
+    	tv_bt_status.setText(MeU.btStatus());
     }
     
-    public void sendImage(View v)
+    private void populateBtDevices(){    	
+    	ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+        		android.R.layout.simple_spinner_item, MeU.listMeUs(getApplicationContext()));
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spn_bluetooth.setAdapter(dataAdapter);
+        updateStatus();
+    }
+    
+    private void connect()
     {
-    	Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+    	if(spn_bluetooth.getSelectedItem() != null)
+    	{
+    		MeU.setupBluetooth(getApplicationContext(), spn_bluetooth.getSelectedItem().toString());
+    	}
+    	updateStatus();
+    }
+    
+//	private void disconnnect() {
+//		MeU.disconnectBt();
+//		updateStatus();		
+//	}
+    
+    private void sendText()
+    {
+    	MeU.sendText(et_text.getText().toString(), Integer.toHexString(picker.getColor()).substring(2));
+    }
+    
+    private void sendImage(int resource)
+    {
+    	Bitmap bmp = BitmapFactory.decodeResource(getResources(), resource);
     	MeU.sendImage(bmp);
     }
 
+	@Override
+	public void onClick(View v) {
+		switch (v.getId())
+		{
+			case R.id.BTN_main_connect:
+				connect();
+				break;
+								
+			case R.id.BTN_main_sendText:
+				sendText();
+				break;
+				
+			case R.id.BTN_main_image1:
+				sendImage(R.drawable.nascent);
+				break;
+				
+			case R.id.BTN_main_image2:
+				sendImage(R.drawable.wwto);
+				break;
+				
+			case R.id.BTN_main_image3:
+				sendImage(R.drawable.heart);
+				break;
+
+			case R.id.BTN_main_image4:
+				sendImage(R.drawable.star);
+				break;
+
+			case R.id.BTN_main_image5:
+				sendImage(R.drawable.skull);
+				break;
+
+			case R.id.BTN_main_image6:
+				sendImage(R.drawable.megaman);
+				break;
+
+			case R.id.BTN_main_image7:
+				sendImage(R.drawable.hearts);
+				break;
+
+			case R.id.BTN_main_image8:
+				sendImage(R.drawable.mushroom_red);
+				break;
+
+			case R.id.BTN_main_image9:
+				sendImage(R.drawable.mushroom_green);
+				break;
+
+			case R.id.BTN_main_image10:
+				sendImage(R.drawable.goomba);
+				break;
+				
+			case R.id.BTN_main_image11:
+				sendImage(R.drawable.fireflower);
+				break;
+
+			case R.id.BTN_main_image12:
+				sendImage(R.drawable.gremlin);
+				break;
+			
+			case R.id.BTN_main_refresh:
+				populateBtDevices();
+				break;
+				
+//			case R.id.BTN_main_disconnect:
+//				disconnnect();
+//				break;
+		}
+	}
 }
--- README.md
+++ README.md
@@ -3,12 +3,20 @@ MeU Android SDK
 
 ### About
 
-**version: 0.0.1**
+**version: 0.0.2**
 
 Simple implementation to connect to a MeU and send text, images or custom data over bluetooth.
 
 What is the [MeU](http://www.themeu.net/)?
 It's a flexible, wearable, programmable LED panel.
+
+### Repository Structure
+
+This repository has 3 folder.
+
+* **SDK-libs** This folder contains 2 jar files needed to incorporate the SDK into your project.
+* **MeU** This folder contains the source code for the MeU SDK, and may be used a reference, or modified as desired.
+* **MeuSample** This folder contains the sample project.
 
 ### Usage
 
@@ -42,14 +50,28 @@ Or if you modify the code running on the MeU you can still use this library to c
 
 `MeU.sendRaw("YOUR CUSTOM STRING HERE");`
 
+There are a few more methods available (such as listing currently available MeUs, getting the bluetooth status etc). The full source and a sample app is bundled in this repository for reference.
+
 That's pretty much it, enjoy!
 
 ### Sample
 
-I've also include a (very poorly designed) sample app to help you along should you get stuck somewhere.
+I've also include a sample app to help you along should you get stuck somewhere. Here's what it looks like:
+
+![Sample App](https://raw.github.com/ankitg/meu-android-sdk/master/MeU_Sample.png)
 
 ### Feedback
 
 This was pretty much hacked together in a couple hours to make development easier for others. If there's enough interest, I'll make time to fix this up by adding proper interfaces for event handling AND a disconnect and clean up method.
 
 Should you find any bugs please feel free to reach me @ ankitg[at]gmail[dot]com
+
+### Credits
+
+All images used in the sample are for educational purposes only and are properties of their respective copyright owners.
+
+The SDK makes use of the [btserial](https://github.com/arduino/BtSerial) library (under the GPL / LGPL licenses).
+
+The sample references the [Holo Color Picker](https://github.com/LarsWerkman/HoloColorPicker) which is under the [Apache License](https://github.com/LarsWerkman/HoloColorPicker/blob/master/LICENSE).
+
+Special thanks to Robert Tu from [MeU](http://www.themeu.net/) for lending me the hardware to make this project possible.
